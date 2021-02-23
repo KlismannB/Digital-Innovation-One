8d0261cf108037532657e5bf808fac4633be5119
Klismann Barros <klismannbarros@eng.ci.ufpb.br>
2021-02-23 20:24:05 -0300

    Initial commit

---
+++ HTML Web Developer/Programação para internet com JavaScript/main.js
@@ -0,0 +1,94 @@
+
+function botao(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+}
+
+function redirecionar() {
+    window.open("https://www.google.com/");
+    window.location.href = "https://www.google.com/"
+}
+
+
+// function soma(n1,n2) {
+//     return n1 + n2;
+// }
+
+// function setReplace(frase, nome, novo_nome) {
+//     return frase.replace(nome, novo_nome);;
+// }
+
+// function validaIdade(idade){
+//     var validar;
+//     if(idade >= 18){
+//         validar = true;
+//     }else{
+//         validar = false;
+//     }
+
+//     return validar;
+// }
+
+// console.log(setReplace("Vai Japão", "Japão", "Brasil"));
+
+// console.log(soma(1,2));
+
+// var idade = prompt("Digite sua idade: ");
+// console.log(validaIdade(idade));
+
+// // var count;
+
+// // var d = new Date();
+
+// // console.log(d);
+
+// // for(count = 0 ; count <= 5; count++) {
+// //     console.log(count);
+// // }
+
+// // while(count < 5){
+// //     console.log(count);
+// //     count++;
+// // }
+
+// // var idade = 18;
+
+// // if(idade >= 18){
+// //     alert("Maior de idade");
+// // }else{
+// //     alert("Menor de idade");
+// // }
+
+// // var lista = ["Maçã", "Pêra", "Laranja"];
+
+// // var fruta = [
+// // {
+// //     nome: "Maçã",
+// //     cor: "Vermelha"
+// // },{
+// //     nome:"Uva",
+// //     cor:"Roxo"
+// // }];
+
+// // console.log(fruta);
+// // console.log(fruta[1].nome);
+
+// // // console.log(lista);
+// // // console.log(lista[0]);
+// // // console.log(lista.length);
+// // // console.log(lista.toString());
+// // // console.log(lista.toString()[0]);
+// // // console.log(lista.join(" - "));
+
+// // // var nome = "Klismann Barros";
+
+// // // //alert("Bem-vindo, " + nome);
+
+// // // var idade = 19;
+// // // var idade2 = 10;
+// // // var frase = "Japão é o melhor time do mundo";
+// // // //alert(nome + " tem " + idade + " anos.");
+// // // //alert(idade + idade2);
+
+// // // //console.log(idade + idade2);
+// // // console.log(frase);
+// // // console.log(frase.replace("Japão", "Brasil"));

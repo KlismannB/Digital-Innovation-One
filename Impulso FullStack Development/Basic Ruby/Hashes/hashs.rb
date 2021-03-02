1f100bb94632ab19981254355a89523771e77f42
Klismann Barros <klismannbarros@eng.ci.ufpb.br>
2021-03-02 13:31:30 -0300

    Added Arrays, Hashes and Input fomr

---
+++ Impulso FullStack Development/Basic Ruby/Hashes/hashs.rb
@@ -0,0 +1,10 @@
+hash = {}
+
+valor = 1
+
+hash = {
+    :valor1 => valor,
+    :valor2 => valor + 1
+}
+
+puts hash
---
+++ Impulso FullStack Development/Basic Ruby/Input/input.rb
@@ -0,0 +1,13 @@
+p ("Digite seu nome: ")
+nome = gets
+
+# Retirar \n
+
+puts nome
+
+nome = nome.chomp
+
+puts nome
+
+
+# nome = nome.gets.chomp  -> Retira o \n automaticamente
